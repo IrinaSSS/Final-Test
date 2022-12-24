51cfb14445dd4fbb83a89d9f10eecc62bf2dd982
Irriiiina <saviris@yandex.ru>
2022-12-24 19:14:45 +0300

    Создана программа для формирования массива где элементы строки с количеством символов меньше или равно трем

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите количество элементов массива");
+
+int N = Convert.ToInt32(ReadLine());
+string[] array = new string[N];
+
+WriteLine("Задайте ваш массив");
+for (int i = 0; i < N; i++)
+{
+    array[i] = ReadLine()!;
+}
+
+string [] newArray (string [] array)
+{
+string[] a = new string[array.Length];
+int j=0;
+for (int i = 0; i < N; i++)
+ {  
+   if (array[i].Length <= 3)
+    {   a[j] = array[i];
+        j++;
+    }
+ }
+ return a;
+}
+
+WriteLine("Новый массив:");
+string [] a = newArray (array);
+for (int i = 0; i < a.Length; i++)
+{
+    Write(a[i] + "\t ");
+}
